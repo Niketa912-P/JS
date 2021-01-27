d48ad42220c42cff23ac34ded2a8ae874e4c7fac
niketa912-p <niketa912@gmail.com>
2021-01-27 16:20:19 -0600

    Redux Actions

--- React/confusion/src/components/DishDetailComponent.js
+++ React/confusion/src/components/DishDetailComponent.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
-const required = (val) => val && val.length;
+
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
 
@@ -27,8 +27,7 @@ class CommentForm extends Component {
 	
 	handleSubmit(values) {
 		this.toggleModal();
-		console.log('Current State is: ' + JSON.stringify(values));
-		alert('Current State is: ' + JSON.stringify(values));
+		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 
 	render() {
@@ -65,7 +64,6 @@ class CommentForm extends Component {
 								model=".author"
 								show="touched"
 								messages={{
-                                    required: 'Required',
 									minLength: 'Must be greater than 2 characters',
 									maxLength: 'Must be 15 characters or less'
 								}}
@@ -107,7 +105,7 @@ function RenderDish({dish}) {
 	);
 }
 
-function RenderComments({comments}) {		
+function RenderComments({comments, addComment, dishId}) {		
 	const commentlist = comments.map((comment) => {
 		return (
 		<div key={comment.id}>
@@ -123,7 +121,7 @@ function RenderComments({comments}) {
 			<ul className="list-unstyled">
 				{commentlist}
 			</ul>
-			<CommentForm />
+			<CommentForm dishId={dishId} addComment={addComment} />
 		</div>
 	);
 }
@@ -146,7 +144,9 @@ const DishDetail = (props) => {
 				</div>
 				<div className="row">
 					<RenderDish dish={props.dish} />
-					<RenderComments comments={props.comments}/>
+					<RenderComments comments={props.comments}
+					addComment={props.addComment}
+					dishId={props.dish.id}/>
 				</div>
 			</div>
 		);
