c0408d17b017ffe7e296a7282b9e1bd4ea80c120
niketa912-p <niketa912@gmail.com>
2021-02-01 23:02:47 -0600

    fetch post comment

--- React/confusion/src/components/DishDetailComponent.js
+++ React/confusion/src/components/DishDetailComponent.js
@@ -29,7 +29,7 @@ class CommentForm extends Component {
 	
 	handleSubmit(values) {
 		this.toggleModal();
-		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+		this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 
 	render() {
@@ -107,7 +107,7 @@ function RenderDish({dish}) {
 	);
 }
 
-function RenderComments({comments, addComment, dishId}) {		
+function RenderComments({comments, postComment, dishId}) {		
 	const commentlist = comments.map((comment) => {
 		return (
 		<div key={comment.id}>
@@ -123,7 +123,7 @@ function RenderComments({comments, addComment, dishId}) {
 			<ul className="list-unstyled">
 				{commentlist}
 			</ul>
-			<CommentForm dishId={dishId} addComment={addComment} />
+			<CommentForm dishId={dishId} postComment={postComment} />
 		</div>
 	);
 }
@@ -165,7 +165,7 @@ const DishDetail = (props) => {
 				<div className="row">
 					<RenderDish dish={props.dish} />
 					<RenderComments comments={props.comments}
-					addComment={props.addComment}
+					postComment={props.postComment}
 					dishId={props.dish.id}/>
 				</div>
 			</div>
